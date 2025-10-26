36ef8c2ef4b6b2f97a0581c926b182e38538c2f8
Konstantin Topaikin <ktopaikin@adnoc.ae>
2025-10-26 18:12:09 +0400

    add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
-const calculateHash = async () => {
-  // Write your code here
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+const calculateHash = async () => { 
+  const hash = createHash('sha256');
+  const readStream = createReadStream('./src/hash/files/fileToCalculateHashFor.txt');
+    
+  await pipeline(readStream, hash);
+    
+  const result = hash.digest('hex');
+  console.log(result);
 };
 
 await calculateHash();
