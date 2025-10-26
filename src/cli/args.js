72546032d69d368e7dbd966240a374611838c96a
Konstantin Topaikin <ktopaikin@adnoc.ae>
2025-10-26 17:33:32 +0400

    add cli subtasks solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-  // Write your code here
+  for (let i = 0; i <= process.argv.slice(2).length; i += 2) {
+    const name = process.argv[i].slice(2);
+    const value = process.argv[i + 1];
+
+    console.log(`${name} is ${value}`)
+  }
 };
 
 parseArgs();
