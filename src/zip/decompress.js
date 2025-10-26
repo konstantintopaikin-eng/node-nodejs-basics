0ddf11ca7a00862d39940383bedd2922c652c588
Konstantin Topaikin <ktopaikin@adnoc.ae>
2025-10-26 19:18:02 +0400

    add compression tasks solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import {
+  createReadStream,
+  createWriteStream,
+} from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-  // Write your code here
+  const source = createReadStream('./src/zip/files/archive.gz');
+  const destination = createWriteStream('./src/zip/files/fileToCompress.txt');
+
+  const unzip = createGunzip()
+
+  pipeline(source, unzip, destination, (err) => {
+    if(err) throw err
+  })
 };
 
 await decompress();
