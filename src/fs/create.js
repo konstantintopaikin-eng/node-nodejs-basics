9ec19825f7202d1807ec4e8a43e8f28be4221bf9
Konstantin Topaikin <ktopaikin@adnoc.ae>
2025-10-26 16:48:09 +0400

    finish file system tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs';
+
 const create = async () => {
-  // Write your code here
+  const text = 'I am fresh and young';
+
+  fs.writeFile('./src/fs/files/fresh.txt', text, { flag: 'wx' } ,err => {
+    if (err) {
+      throw new Error('FS operation failed')
+    } 
+  })
 };
 
 await create();
